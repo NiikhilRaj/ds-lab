77533f505634f17ab545291f9a9daef934763530
Your Name <nikhilraj13733@gmail.com>
2024-07-30 20:32:59 +0530

    mm

--- P2.2_2085.c
+++ P2.2_2085.c
@@ -3,6 +3,21 @@
 
 int main()
 {
+    int n, count;
+
+    printf("Enter the length of the array: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)calloc(n,sizeof(int));
+
+    printf("Enter the elements: ");
     
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+
+
     return 0;
 }
